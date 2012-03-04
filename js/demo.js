5550e077ba7520f6fdcd5ef9bff9ba71dfb8d298
Justas <justukas@gmail.com>
2012-03-05 00:04:10 +0200

    Cleanup. Implemented box spawning on click

--- js/demo.js
+++ js/demo.js
@@ -1,20 +1,34 @@
-var player = function(){
-	this.object = null;
-	this.canJump = false;
-};
-var world = null;
-var bodies = [];
-var DOMbodies = [];
+var DEMO = {
 
-var DEMO = {}
+    world        : null,
+    bodies       : [],
+    DOMbodies    : [],
+    up           : false,
+    timeStep     : 1.0/60,
+    iterations   : 1,
+    refreshSpeed : 10,
+    mouseX       : 0,
+    moyseY       : 0,
 
-function step() {
-	
-	var timeStep = 1.0/60;
-	var iteration = 4;
-	world.Step(timeStep, iteration);
+    start : function() {
 
-    var that = this;
+        this.initWorld();
+        this.initScene();
+        this.refreshWorld(); //loops
+
+    },
+
+    refreshWorld : function () {
+
+        if(this.up) {
+            var vel = this.bodies[this.bodies.length-1].GetLinearVelocity();
+            vel.y -= 50;
+            this.bodies[this.bodies.length-1].SetLinearVelocity(vel);
+        }
+
+        this.world.Step(this.timeStep, this.iterations);
+
+        var that = this;
         jQuery(this.bodies).each(function(key){
 
             var style = 'rotate(' + (this.m_rotation0 * 57.2957795) + 'deg)';
@@ -30,48 +44,88 @@ function step() {
 
         });
 
-	setTimeout('step()', 10);
-}
+        setTimeout('DEMO.refreshWorld()', this.refreshSpeed);
 
-function initGame(){
-	
-	player.object = createBox(world, 0, 0, 5, 5, false, 'player');
+    },
 
-    var that = this;
+    initScene : function() {
 
-    jQuery('#view div').each(function() {
+        var that = this;
 
-        var pos = jQuery(this).position();
+        jQuery('div').each(function() {
 
-        var prop = new Object();
-        prop.top    = pos.top;
-        prop.left   = pos.left;
-        prop.width  = jQuery(this).width();
-        prop.height = jQuery(this).height();
+            var pos = jQuery(this).position();
 
-        var type = jQuery(this).hasClass('st');
+            var prop = new Object();
+            prop.top    = pos.top;
+            prop.left   = pos.left;
+            prop.width  = jQuery(this).width();
+            prop.height = jQuery(this).height();
 
-        that.bodies.push( createBox(
-            world,
-            pos.left,
-            pos.top,
-            prop.width / 2,
-            prop.height / 2,
-            type
-        ));
+            var type = jQuery(this).hasClass('st');
 
-        that.DOMbodies.push(this);
+            that.bodies.push( createBox(
+                that.world,
+                pos.left,
+                pos.top,
+                prop.width / 2,
+                prop.height / 2,
+                type
+            ));
 
-    });
+            that.DOMbodies.push(this);
 
+        });
+
+        jQuery(document).keypress(function() {
+            that.up = true;
+        });
+        jQuery(document).keyup(function() {
+            that.up = false;
+        });
+
+        jQuery(document).click(function() {
+
+            that.bodies.push( createBox(
+                that.world,
+                that.mouseX - (25/2),
+                that.mouseY - (25/2),
+                25,
+                25,
+                false
+            ));
+
+            var newBox = jQuery('<div class="cube"></div>');
+            jQuery(newBox).css({
+                'left' : that.mouseX - (25/2),
+                'top' : that.mouseY - (25/2)
+            })
+            that.DOMbodies.push(newBox);
+            jQuery('body').append(newBox);
+
+        });
+
+        jQuery(document).mousemove(function(e){
+            that.mouseX = e.pageX;
+            that.mouseY = e.pageY;
+        });
+
+    },
+
+    initWorld : function() {
+
+        var worldAABB = new b2AABB();
+        worldAABB.minVertex.Set(-10000, -10000);
+        worldAABB.maxVertex.Set(10000, 10000);
+        var gravity = new b2Vec2(0, 100);
+        var doSleep = true;
+        this.world = new b2World(worldAABB, gravity, doSleep);
+
+    }
 }
 
 jQuery(document).ready(function() {
-    world = createWorld();
-
-	initGame();
-	step();
-
+    DEMO.start();
 });
 
 
